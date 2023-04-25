9fd4f7fd333e2bb9e55c35c7f5f9bc99a1fd6238
JonLuca DeCaro <jonluca.decaro@gmail.com>
2023-04-25 15:24:57 -0700

    change name, add logs, bump versions

--- examples/src/demo.rs
+++ examples/src/demo.rs
@@ -7,6 +7,7 @@ pub fn demo() {
     let dimension = 64;
 
     // make sample points
+    println!("Making samples");
     let mut samples = Vec::with_capacity(n);
     let normal = Normal::new(0.0, 10.0).unwrap();
     for _i in 0..n {
@@ -16,18 +17,26 @@ pub fn demo() {
         }
         samples.push(sample);
     }
+    println!("Samples made");
 
+    println!("samples: {:?}", samples.len());
     // init index
+    println!("Init index");
     let mut index = hora::index::hnsw_idx::HNSWIndex::<f32, usize>::new(
         dimension,
         &hora::index::hnsw_params::HNSWParams::<f32>::default(),
     );
+    println!("Index inited");
+    // add points
+    println!("Adding points");
     for (i, sample) in samples.iter().enumerate().take(n) {
         // add point
         index.add(sample, i).unwrap();
     }
-    index.build(hora::core::metrics::Metric::Euclidean).unwrap();
-
+    println!("Points added");
+    println!("Building index");
+    index.build(hora::core::metrics::Metric::CosineSimilarity).unwrap();
+    println!("Index built");
     let mut rng = thread_rng();
     let target: usize = rng.gen_range(0..n);
     // 523 has neighbors: [523, 762, 364, 268, 561, 231, 380, 817, 331, 246]
